bfa79055ba040c6751d18a31f77a5f032d312982
VictorMoya <vjmoyaparedes@gmail.com>
2023-06-21 01:11:00 -0300

    feat: instalacion de dependencias y colocar providers

--- src/main.tsx
+++ src/main.tsx
@@ -7,17 +7,23 @@ import { BrowserRouter } from "react-router-dom";
 import { AuthProvider } from "./context/authContext.tsx";
 import { AlertProvider } from "./context/alertContext.tsx";
 import theme from './theme';
+import { MarvelProvider } from "./context/marvelContext.tsx";
+import { ConfigProvider } from "./context/configContext.tsx";
 
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <React.StrictMode>
     <ThemeProvider theme={theme}>
       <BrowserRouter>
+      <ConfigProvider>
+      <MarvelProvider>
         <AlertProvider>
           <AuthProvider>
             <CssBaseline />
             <App />
           </AuthProvider>
         </AlertProvider>
+        </MarvelProvider>
+        </ConfigProvider>
       </BrowserRouter>
     </ThemeProvider>
   </React.StrictMode>
